da50c5c76824df5d8af5aecb2f4a2764fdb74482
arehf <arehfuss@gmail.com>
2022-06-29 18:48:26 -0700

    Changed the names and wiped the files for a fresh look at making classes.

--- ModelViewControl/Program.cs
+++ ModelViewControl/Program.cs
@@ -1,22 +1,2 @@
 ﻿using Models;
 Console.WriteLine("Hello, World!");
-// string[] love = {"Sex","Love","Fornication","Truth","Focus","Compassion"};
-
-// for(int i = 0; i < love.Length; i++){
-//   Console.WriteLine($"{love[i]}");
-// }
-
-// Array.Sort(love);
-// Console.WriteLine();
-// foreach(string Thing in love)
-// {
-//   Console.WriteLine($"{Thing}");
-// }
-
-FromModel manager = new FromModel();
-FromModel manager2 = new FromModel();
-manager.employeeName = "Tarzan";
-manager2.employeeName = "peter";
-string n = manager.employeeName;
-Console.WriteLine(n);
-Console.WriteLine(manager2.employeeName);
---
+++ ModelViewControl/UI
@@ -0,0 +1,19 @@
+namespace UI;
+
+
+public class MainMenu 
+{
+  public void Start()
+  {
+    do {
+      Console.WriteLine("Welcome to Ticket Manager");
+      Console.WriteLine("[1] Manager Login");
+      Console.WriteLine("[2] Employee Login");
+      Console.WriteLine("[x]");
+      string input = Console.ReadLine();
+  
+
+
+    }
+  }
+}
