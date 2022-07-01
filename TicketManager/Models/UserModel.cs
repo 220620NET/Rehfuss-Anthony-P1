86b40c7527bc84a8e25e2fc5c96b45e851d33106
arehf <arehfuss@gmail.com>
2022-07-01 15:04:25 -0700

    added User class

---
+++ TicketManager/Models/UserModel.cs
@@ -0,0 +1,12 @@
+namespace Models;
+
+
+
+public class User
+{
+  public int UserId {get;set;}
+  public string UserName {get; set;} = "";
+  public string password {get; set;} = "";
+  public string role {get; set;} = "";
+
+}
--- TicketManager/UI/Program.cs
+++ TicketManager/UI/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//namespace UI;
+using Models;
+
 Console.WriteLine("Hello, World!");
+User manager = new User();
+manager.UserId = 123;
+manager.UserName = "tom";
+Console.WriteLine(manager.UserName);
+
+
+
