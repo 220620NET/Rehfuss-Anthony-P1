df029f6d2e33bc25254632c1b8313b08c033373e
arehf <arehfuss@gmail.com>
2022-07-02 14:42:22 -0700

    Added Ticket Models Class

---
+++ TicketManager/Models/TicketModel.cs
@@ -0,0 +1,17 @@
+using CustomExceptions;
+
+namespace models;
+
+public class TicketModels
+{
+   
+   public int Id {get; set;}
+   public string Author {get; set;}
+   public string Resolover {get; set;}
+   public string Description {get; set;}
+   public string status {get; set;}
+   public decimal Amount {get; set;}
+  
+
+}
+
--- TicketManager/Models/UserModel.cs
+++ TicketManager/Models/UserModel.cs
@@ -5,7 +5,7 @@ namespace Models;
 public class User
 {
   public int UserId {get;set;}
-  public string UserName {get; set;} = "";
+  public string UserName {get; set;} = "";  //this gets rid of the null warnings
   public string password {get; set;} = "";
   public string role {get; set;} = "";
 
