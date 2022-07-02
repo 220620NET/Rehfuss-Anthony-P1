689c3606fc1a562f868769e7134a8d119bc4cbc3
arehf <arehfuss@gmail.com>
2022-07-02 16:04:55 -0700

    Changed UserModel

---
+++ TicketManager/DataAccess/TicketReimbursementRepository.cs
@@ -0,0 +1,13 @@
+using Models;
+using System.Text.Json;
+namespace DataAccess;
+
+public class TicketReimbursementRepository
+{
+  //this is class saves the all aspects of our Ticket Reimbursement app 
+  // so far it is going to be saved to JSON 7/2/2022
+
+  
+
+
+}
--- TicketManager/Models/UserModel.cs
+++ TicketManager/Models/UserModel.cs
@@ -1,12 +1,68 @@
+using CustomExceptions;
 namespace Models;
 
 
 
 public class User
 {
-  public int UserId {get;set;}
-  public string UserName {get; set;} = "";  //this gets rid of the null warnings
+
+   public User(string UserName, string role) 
+   {
+      this.Name = UserName="";
+      this.role = role="";
+   }
+   
+
+  private string _name="";
+  public string Name
+{
+       get { return _name;}
+
+       set
+       {
+        if (String.IsNullOrWhiteSpace(value))
+        {
+          throw new InputInvalidException("Name must not be empty");
+        }
+        else //optional
+        {
+          _name = value;
+        }
+       }
+
+       
+}
+
+
+
+  public int Id {get;set;}
+ // public string Name {get; set;} = "";  //this gets rid of the null warnings
   public string password {get; set;} = "";
   public string role {get; set;} = "";
 
+  public override string ToString()
+  {
+    return $"Name: {this.Name} \nrole: {this.role}";
+  }
+
+  public override bool Equals(object? obj)
+  {
+      if(obj.GetType() == this.GetType())
+      {
+        User usertoCompare = (User) obj;
+        if(this.Id == usertoCompare.Id && this.Name == usertoCompare.Name )
+        {
+           return true;
+        }
+        return false;
+      }
+      else
+      {
+         return false;
+      }
+
+  }
+
 }
+
+
